3cf8f607e521d8aa1a06e2c18eab53cb9fd408d3
sinan-prvt <mohamedsinan9400@gmail.com>
2025-09-01 12:45:59 +0530

    updated1

---
+++ Home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def booking(request):
+    return render(request, 'booking.html')
+
+def doctors(request):
+    return render(request, 'doctors.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def department(request):
+    return render(request, 'department.html')
