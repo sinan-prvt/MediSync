09a3f57987988fd8cb7e9587863615df2272be43
sinan-prvt <mohamedsinan9400@gmail.com>
2025-09-01 16:20:28 +0530

    updated2

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Department, Doctors
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -11,10 +12,16 @@ def booking(request):
     return render(request, 'booking.html')
 
 def doctors(request):
-    return render(request, 'doctors.html')
+    dict_doc = {
+        'doc' : Doctors.objects.all()
+    }
+    return render(request, 'doctors.html', dict_doc)
 
 def contact(request):
     return render(request, 'contact.html')
 
 def department(request):
-    return render(request, 'department.html')
+    dict_dept = {
+        'dept' : Department.objects.all()
+    }
+    return render(request, 'department.html', dict_dept)
--- Hospital/settings.py
+++ Hospital/settings.py
@@ -125,3 +125,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = '/media/'
